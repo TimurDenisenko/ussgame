9432308ddb9afd813685986cf4c97a723394a327
opilane <opilane@HP-CZC2349HTY>
2023-05-15 12:00:19 +0300

    Class point

---
+++ Mang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ussgame
+{
+    public class Mang
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
